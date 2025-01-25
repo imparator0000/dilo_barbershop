5ec200e2c8b37b926869c5ec08d8abde7f3a96b7
imparator0000 <imparatorimprator@gmail.com>
2025-01-25 13:29:53 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Galeriye daha fazla fotoğraf ya da video eklemek için burayı kullanabilirsiniz.
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Dilo Barbershop web sitesi çalışıyor!");
+});
